f44aabeeabe3bdb44e9c5f9bcc58cf9415638004
LinAung-Thet <laungthet@gmail.com>
2025-08-20 18:09:03 +1200

    Updated AddTwoNumbers.cpp
    - Updated to handle big numbers.

--- AddTwoNumbers.cpp
+++ AddTwoNumbers.cpp
@@ -13,32 +13,27 @@ struct ListNode{
 class Solution{
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == nullptr) return l2;
-        if(l2 == nullptr) return l1;
+        ListNode* head = new ListNode(0);
+        ListNode* curr = head;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
 
-        long multiplier = 1;
-        long num1 = 0, num2 = 0, sum = 0;
-        while(l1){
-            num1 += l1->val * multiplier;
-            multiplier *= 10;
-            l1 = l1->next;
-        }
-        multiplier = 1;
-        while(l2){
-            num2 += l2->val * multiplier;
-            multiplier *= 10;
-            l2 = l2->next;
-        }
-        sum = num1 + num2;
+            carry = sum / 10;
 
-        ListNode* head = new ListNode(0, new ListNode(0));
-        ListNode* prev = head;
-        while(sum > 0) {
-            prev->next = new ListNode(sum % 10);
-            prev = prev->next;
-            sum = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
         }
-        return head->next;
+
+        return head->next ? head->next : head;
     }
 };
 

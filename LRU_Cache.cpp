e26d163c5a3c2568e6a081513008d29b76e1d87b
LinAung-Thet <laungthet@gmail.com>
2025-09-15 18:33:59 +1200

    Added the implementation of LRU_Cache.cpp

--- LRU_Cache.cpp
+++ LRU_Cache.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 class Cache {
@@ -7,10 +8,67 @@ private:
         int value;
         Node* next;
         Node* prev;
+        Node(int v) : value(v), next(nullptr), prev(nullptr){}
     };
 
+    Node* head = new Node(-1);
+    Node* tail = new Node(-1);
+    int capacity;
+
+    unordered_map<int, Node*> data;
+
+    void deleteNode(Node* node) {
+        Node* prevNode = node->prev;
+        Node* nextNode = node->next;
+
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+    }
+
+    void addNode(Node* node) {
+        Node* nextNode = head->next;
+
+        head->next = node;
+        node->prev = head;
+        node->next = nextNode;
+        nextNode->prev = node;
+    }
+
 public:
-    int get() {
-        return 0;
+    Cache(int _capacity) {
+        capacity = _capacity;
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(int key) {
+        if (data.find(key) != data.end()) {
+            Node* node = data[key];
+            int result = node->value;
+
+            // remove the existing node and add it at the beginning.
+            data.erase(key);
+            deleteNode(node);
+
+            data[key] = node;
+            addNode(node);
+
+            return result;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+        if (data.find(key) != data.end()) {
+            Node* node = data[key];
+            data.erase(key);
+            deleteNode(node);
+        }
+        if (data.size() >= capacity) {
+            data.erase(tail->prev->value);
+            deleteNode(tail->prev);
+        }
+        addNode(new Node(value));
+        data[key] = head->next;
     }
 };

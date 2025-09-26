4e85c37526c211e3240c9603f79672e78955cdd8
LinAung-Thet <laungthet@gmail.com>
2025-09-27 09:47:26 +1200

    Updated OddEvenLL.cpp
    - Added test cases.

--- OddEvenLL.cpp
+++ OddEvenLL.cpp
@@ -1,3 +1,8 @@
+// https://leetcode.com/problems/odd-even-linked-list/description/?envType=problem-list-v2&envId=linked-list
+
+#include <iostream>
+using  namespace std;
+
 struct ListNode {
     int val;
     ListNode *next;
@@ -33,3 +38,31 @@ public:
         return first;
     }
 };
+
+int main() {
+    Solution solution;
+    ListNode* head;
+
+    cout << "Test case 1" << endl;
+    cout << "Input   : 1 2 3 4 5" << endl;
+    cout << "Expected: 1 3 5 2 4" << endl;
+    cout << "Result  :";
+    head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    head = solution.oddEvenList(head);
+    while (head) {
+        cout << " " << head->val;
+        head = head->next;
+    }
+    cout << endl << endl;
+
+    cout << "Test case 2" << endl;
+    cout << "Input   : 2 1 3 5 6 4 7" << endl;
+    cout << "Expected: 2 3 6 7 1 5 4" << endl;
+    cout << "Result  :";
+    head = new ListNode(2, new ListNode(1, new ListNode(3, new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(7)))))));
+    head = solution.oddEvenList(head);
+    while (head) {
+        cout << " " << head->val;
+        head = head->next;
+    }
+}
